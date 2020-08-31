4d18a0536c0ad6323e6c0ae9a0abcb67d5bb33ee
Ilona_Mazur <i.mazurr2d2@gmail.com>
2020-08-31 20:56:59 +0200

    cleaned oop3 day_name called by day_number; code works

--- class_oop_3.py
+++ class_oop_3.py
@@ -1,4 +1,4 @@
-#def dayByNumber(weekday):
+# def dayByNumber(weekday):
 #    if weekday == 0:
 #        retyrn "Monday"
 #    if weekday ==
@@ -7,29 +7,31 @@
 # v2
 
 import calendar
+
 list(calendar.day_name)
-('Monday')
 
 print(calendar.day_name[3])
 
-
-#v3
+# v3
 DayOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 print(DayOfTheWeek[1])
 
-#v4 from class
+
+# v4 from class
 def get_weekday_bynumb(weekday: int) -> str:
-    days ={ 1 : 'Mon'
-            2 : 'Tue'
-            3 : 'Wed'
-            4 : 'Thu'
-            5 : 'Fri'
-            6 : 'Sat'
-            7 : 'Sun'}
+    days = {1: 'Mon',
+            2: 'Tue',
+            3: 'Wed',
+            4: 'Thu',
+            5: 'Fri',
+            6: 'Sat',
+            7: 'Sun'}
     return days.get(weekday, 'incorrect value')
+
 
 def main():
     print(get_weekday_bynumb(1))
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     main()
